3fad4241caf6eec099f71e72f61d343fc037c01e
Carlos <carlos.alfonso.montero@gmail.com>
2021-03-28 19:24:52 -0400

    Completed assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+let h2 = document.createElement('h2')
+h2.textContent = "Everything you want in custom design..."
+document.querySelector('body').appendChild(h2)
+
+let h3 = document.createElement('h3')
+h3.textContent = "Visit us..."
+document.querySelector('body').appendChild(h3)
